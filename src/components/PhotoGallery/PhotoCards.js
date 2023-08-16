c02aab0b3e35efa7f7afbd5cc809b079488c7126
Plain Demon <89330268+Plaindemon@users.noreply.github.com>
2023-08-16 17:35:02 -0600

    fixed some styling on the gallery page

--- src/components/PhotoGallery/PhotoCards.js
+++ src/components/PhotoGallery/PhotoCards.js
@@ -7,66 +7,66 @@ function PhotoCards() {
   return (
     <div className="cards">
       {/* <h1>My current services</h1> */}
-      <div className="cards__container">
-        <div className="cards__wrapper">
-          <ul className="cards__items">
+      <div className="cards-container">
+        <div className="cards-wrapper">
+          <ul className="cards-items">
             <PhotoCardItem
               src="images/jaxson-happy.jpeg"
               text="Dog Walking"
-              label="Adventure"
+              label="Jaxson"
             //   path="/walks"
             />
             <PhotoCardItem
               src="images/jaxson-water.jpeg"
               text="Dog Hiking"
-              label="Adventure"
+              label="Jaxson"
             //   path="/hikes"
             />
             <PhotoCardItem
               src="images/Beasly.jpeg"
               text="Dog Hiking"
-              label="Adventure"
+              label="Beesly"
             //   path="/hikes"
             />
           </ul>
 
-          <ul className="cards__items">
+          <ul className="cards-items">
             <PhotoCardItem
               src="images/mccoy.jpeg"
               text="House Sitting"
-              label="Adventure"
+              label="McCoy"
             //   path="/sitting"
             />
             <PhotoCardItem
               src="images/charlie-fall.jpeg"
               text="Drop-In Visits"
-              label="Adventure"
+              label="Charlie"
             //   path="/drop-in"
             />
             <PhotoCardItem
               src="images/finn-sunny.jpeg"
               text="Dog Hiking"
-              label="Adventure"
+              label="Finn"
             //   path="/hikes"
             />
           </ul>
-          <ul className="cards__items">
+          <ul className="cards-items">
             <PhotoCardItem
               src="images/jaxson-sunflower.jpeg"
               text="Vet Taxi Service"
-              label="Adventure"
+              label="Jaxson"
             //   path="/vet-taxi"
             />
             <PhotoCardItem
               src="images/piper.jpeg"
               text="Doggy Daycare"
-              label="Adventure"
+              label="Piper"
             //   path="/daycare"
             />
             <PhotoCardItem
               src="images/maverick.jpeg"
               text="Dog Hiking"
-              label="Adventure"
+              label="Maverick"
             //   path="/hikes"
             />
           </ul>
